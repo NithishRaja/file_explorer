cc731b1c07cea57c63f549f602c6fea958cbd133
nithish <nithish98raja@gmail.com>
2021-10-13 19:31:55 +0530

    add code to traverse list in terminal

--- main.cpp
+++ main.cpp
@@ -18,29 +18,10 @@
 // Set namespace
 using namespace std;
 
-int main(){
-  // Initialise object to parse path
-  PathParser pp;
-  // Initialise buffer to hold home path
-  struct passwd *pw = getpwuid(getuid());
-  char homedir[256];
-  strcpy(homedir, pw->pw_dir);
-  
-  // char currdir[FILENAME_MAX]; //create string buffer to hold path
-  // getcwd( currdir, FILENAME_MAX );
-  // cout<<currdir<<endl;
-  // strtok(currdir, "\n");
-
-  // Create buffer to hold path
-  char currdir[FILENAME_MAX];
-  getcwd( currdir, FILENAME_MAX );
-  // Parse path
-  string temp(currdir);
-  temp = pp.parse(temp);
-  strcpy(currdir, temp.c_str());
-
-  vector<struct file_info_hr> list = get_dir_content(currdir);
-
+void print_list(vector<struct file_info_hr> list){
+  // Clear screen and place cursor at top left
+  cout<<"\033[2J\033[1;1H";
+  // Iterate over list
   for(int i=0;i<list.size();++i){
     cout.width(12);
     cout<<left<<list[i].permission;
@@ -56,5 +37,102 @@ int main(){
     cout<<left<<list[i].name;
     cout<<endl;
   }
+}
 
+int main(){
+  // Initialise object to parse path
+  PathParser pp;
+  // Initialise buffer to hold home path
+  struct passwd *pw = getpwuid(getuid());
+  char homedir[FILENAME_MAX];
+  strcpy(homedir, pw->pw_dir);
+
+  // Create buffer to hold path
+  char currdir[FILENAME_MAX];
+  getcwd(currdir, FILENAME_MAX);
+  // Parse path
+  string temp(currdir);
+  temp = pp.parse(temp);
+  strcpy(currdir, temp.c_str());
+  // Call function to get list
+  vector<struct file_info_hr> list = get_dir_content(currdir);
+
+  // Initialise port
+  int serial_port = open("/dev/tty", O_RDWR);
+  // Check for errors
+  if (serial_port < 0) {
+    printf("Error %i from open: %s\n", errno, strerror(errno));
+  }
+  // Initialise struct for handling terminal io
+  struct termios tty_backup, tty_current;
+  // Get current terminal config
+  if(tcgetattr(serial_port, &tty_backup) != 0) {
+      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+  }
+  tty_current = tty_backup;
+  // Update terminal config
+  tty_current.c_lflag &= ~ICANON;
+  tty_current.c_lflag &= ~ECHO;
+  tty_current.c_lflag &= ~ECHOE;
+  tty_current.c_lflag &= ~ECHONL;
+  // Update terminal with new config
+  if (tcsetattr(serial_port, TCSANOW, &tty_current) != 0) {
+      printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+  }
+
+  // Initialise flag to track current mode
+  bool command_mode = false;
+
+  // Get window dimensions
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+  int max_row = w.ws_row;
+  int max_col = w.ws_col;
+  // Get list size
+  int list_size = list.size();
+
+  // Call function to print list of files and directories
+  print_list(list);
+  // Move cursor back to top left
+  cout<<"\033[1;1H";
+  // Initialise variable to hold y coordinate of cursor
+  int y_coord = 1;
+
+  // Start REPL
+  while(true){
+    // Read input
+    char ch = getchar();
+    if(ch == ':'){
+      cout<<"\033["<<max_row<<";1H";
+      cout<<":";
+    }else if(ch == 'k'){
+      cout<<'k';
+    }else if(ch == 'l'){
+      cout<<'l';
+    }else if(ch == 0x7f){
+      cout<<"back";
+    }else if(ch == 10){
+      cout<<"selected: "<<list[y_coord-1].name;
+    }else if(ch == 65){
+      // cout<<"up";
+      if(y_coord>0){
+        --y_coord;
+        cout<<"\033[A";
+      }
+    }else if(ch == 66){
+      // cout<<"down";
+      if(y_coord<max_row-1 && y_coord<list_size){
+        ++y_coord;
+        cout<<"\033[B";
+      }
+    }
+  }
+
+  // Reset to old config
+  if (tcsetattr(serial_port, TCSANOW, &tty_backup) != 0) {
+    printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+  }
+  // Close port
+  close(serial_port);
+  return 0;
 }
